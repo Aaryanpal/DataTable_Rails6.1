03a7ecade1b4286eacb338deab831c55ebd9e949
Aryan Pal <aryan.pal0ppq@gmail.com>
2023-01-12 13:11:06 +0530

    updated the Env File

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,8 +4,8 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
-  config.cache_classes = true
-
+  config.cache_classes = false
+  config.hosts << "datatablerails61-production.up.railway.app"
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
